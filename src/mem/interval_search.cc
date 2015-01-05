e2d48b5db6c0cf33771dab879c6c8f581e9ff316
Tomislav Ljubej <sporbie@gmail.com>
2015-01-05 20:19:03 +0100

    Implemented match_next_char

---
+++ src/mem/interval_search.cc
@@ -0,0 +1,64 @@
+#include "interval_search.h"
+
+enum Side { LEFT, RIGHT };
+
+Index bsearch(char c, const ReferenceString &ref, const MatchInterval &p,
+              Side s) {
+  Index l = p.from;
+  Index r = p.to;
+  Index t;
+  short lessequal = 0;
+  if (s == LEFT) {
+    t = l;
+    lessequal = 1;
+  } else {
+    t = r;
+  }
+
+  if (c == ref.s(ref.sa(t) + p.matched)) {
+    return t;
+  }
+
+  while (r - l > 1) {
+    Index m = (l + r) / 2;
+    if (c < ref.s(ref.sa(m) + p) + lessequal) {
+      r = m;
+    } else {
+      l = m;
+    }
+  }
+
+  if (s == LEFT) {
+    return r;
+  } else {
+    return l;
+  }
+}
+
+namespace mem {
+
+MatchInterval match_next_char(const ReferenceString &ref,
+                              const std::string &query, Index query_pos,
+                              const MatchInterval &prev) {
+  MatchInterval newm(-1, 0, 0);
+
+  char c = query[query_pos];
+
+  if (c != ref.s(ref.sa() + prev.matched)) {
+    return newm;
+  }
+
+  Index l, r;
+  l = bsearch(c, ref, prev, LEFT);
+  r = bsearch(c, ref, prev, RIGHT);
+
+  if (l <= r) {
+    newm.matched = prev.matched + 1;
+    newm.from = l;
+    newm.to = r;
+  }
+
+  return newm;
+}
+
+} // namespace mem
