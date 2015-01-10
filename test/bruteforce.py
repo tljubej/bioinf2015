b8deb42208c799291626c24860719fe9d36f7fb8
Tomislav Ljubej <sporbie@gmail.com>
2015-01-10 16:12:21 +0100

    Bruteforce checker is now much faster

--- test/bruteforce.py
+++ test/bruteforce.py
@@ -1,5 +1,14 @@
 import sys
 
+
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
+
 if __name__ == '__main__':
     fin = open(sys.argv[1], 'r')
     finq = open(sys.argv[2], 'r')
@@ -12,12 +21,12 @@ if __name__ == '__main__':
     sq = finq.read().strip()
     lsq = len(sq)
 
-    mems = []
+    # mems = []
+    #
+    # for i in range(len(s)):
+    #     # if i%100000 == 0:
+    #     #     print(i)
+    #     if s[i:i+lsq] == sq:
+    #         mems.append(i)
 
-    for i in range(len(s)):
-        # if i%100000 == 0:
-        #     print(i)
-        if s[i:i+lsq] == sq:
-            mems.append(i)
-
-    print(mems)
+    print(list(find_all(s, sq)))
